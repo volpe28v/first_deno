02cb3c700189e9287d940459290d3804572af237
volpe28v <naoki.koda@gmail.com>
2021-10-06 09:43:09 +0900

    try import and parse

---
+++ parse_date_with_deps.ts
@@ -0,0 +1,4 @@
+import { parse } from "./deps.ts"
+const date = parse("2021-10-06", "yyyy-MM-dd");
+
+console.log(date)
--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 new Deno.Buffer()
+
+
+import { assertEquals } from "https://deno.land/std@0.110.0/testing/asserts.ts";
+
+assertEquals("hello", "hello");
+assertEquals("world", "world");
+
+console.log("Asserted! ✓");
