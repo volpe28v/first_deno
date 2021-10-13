aa3501e33bf6172bc1eefb33cd51a2e594bf1cc4
volpe28v <naoki.koda@gmail.com>
2021-10-13 10:05:54 +0900

    add get test

--- oak_greet_test.ts
+++ oak_greet_test.ts
@@ -12,3 +12,24 @@ Deno.test({
   sanitizeResources: false,
   sanitizeOps: false,
 });
+
+Deno.test({
+  name: 'GET /greet?name=hoge に対して、ステータス・コード200で "Hello hoge!!" が返ってくること',
+  fn: async () => {
+    const request = await superoak(app);
+    await request.get("/greet?name=hoge").expect(200).expect("Hello hoge!!");
+  },
+  sanitizeResources: false,
+  sanitizeOps: false,
+});
+
+Deno.test({
+    name: 'GET / に対して、ステータス・コード404が返ってくること',
+    fn: async () => {
+      const request = await superoak(app);
+      await request.get("/").expect(404);
+    },
+    sanitizeResources: false,
+    sanitizeOps: false,
+  });
+  
